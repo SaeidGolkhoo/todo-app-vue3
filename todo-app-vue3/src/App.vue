7cbfec47a41825c0ed6b74de99ab12c148202835
Maryam Ghoreishi <maryamghoreishi71@hotmail.com>
2024-01-28 20:01:54 +0100

    I added Modal for AddingForm

--- todo-app-vue3/src/App.vue
+++ todo-app-vue3/src/App.vue
@@ -1,5 +1,12 @@
 <template>
-  <button type="button" class="btn btn-info">Add</button>
+  <button
+    type="button"
+    class="btn btn-info"
+    data-bs-toggle="modal"
+    data-bs-target="#addModal"
+  >
+    Add
+  </button>
   <table class="table table-dark table-striped text-center text-capitalize">
     <thead>
       <tr>
@@ -26,6 +33,38 @@
       </tr>
     </tbody>
   </table>
+
+  <!-- The Modal -->
+  <div class="modal fade" id="addModal">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <!-- Modal Header -->
+        <div class="modal-header">
+          <h4 class="modal-title">Add Todo</h4>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+          ></button>
+        </div>
+
+        <!-- Modal body -->
+        <div class="modal-body">
+          <input
+            type="text"
+            class="form-control mt-3"
+            placeholder="Task Name"
+          />
+          <input type="text" class="form-control mt-3" placeholder="How Long" />
+          <div class="d-grid">
+            <button type="button" class="btn btn-block btn-success mt-3">
+              Add Todo
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup></script>
